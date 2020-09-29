11e87c33373900e3ff4f066b4d6e0830c8a8ce4c
ahmed-mehanna <ahmadabobakr1@gmail.com>
2020-09-30 01:48:56 +0200

    Registeration v1

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,40 +1,30 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-
-
-<div class="container" style="margin-top: 30px">
-    <h1 class="alert alert-warning" role="alert">Verification</h1>
-        <div class="form-control">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-        </div>
-
+@extends('components.app')
+@section('content')
+    <div class="container" style="padding-top: 40px; min-height: 534px;">
+        <h1 class="alert alert-info" role="alert">Verification</h1>
+        <p class="lead">
+            Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+        </p>
         @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
                 <strong class="alert">{{ __('A new verification link has been sent to the email address you provided during registration.') }}</strong>
             </div>
         @endif
-
-        <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
+        <div class="mt-4 mb-5 flex items-center justify-between">
+            <form class="d-inline-block mr-3" method="POST" action="{{ route('verification.send') }}">
                 @csrf
-
                 <div>
-                    <button type="submit" class="btn btn-info" onclick="alert('\nA new verification link has been sent to the email address\n\nPlease check your email ');">>
-                        {{ __('Resend Verification Email') }}
+                    <button type="submit" class="btn btn-primary" style="font-size: 18px;">
+                        Resend Verification Email
                     </button>
                 </div>
             </form>
-
-            <form method="POST" action="{{ route('logout') }}">
+            <form class="d-inline-block mt-3" method="POST" action="{{ route('logout') }}">
                 @csrf
-
-                <button type="submit" class="btn btn-danger">
+                <button type="submit" class="btn btn-danger" style="font-size: 18px;">
                     {{ __('Logout') }}
                 </button>
             </form>
         </div>
-
-</div>
+    </div>
+@endsection
